41846fac810018a7b3070417f8b4ca820bd59a29
Jonathan Muñoz <jonathan2712emr@gmail.com>
2020-05-16 22:49:36 -0500

    Prueba de cambios en lat y lng con console.log

--- Firebase/amigos/scripts/index.js
+++ Firebase/amigos/scripts/index.js
@@ -30,12 +30,17 @@ const configurarMenu = (user) => {
 
 const obtieneAmigos = (data) => {
 
+    console.log('todos los usuarios:')
+    console.log(data);
+
     var map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 21.152639, lng: -101.711598 },
         zoom: 14,
     });
 
     data.forEach(doc => {
+        console.log('usuario por usuario:')
+        console.log(doc);
         informacion = new google.maps.InfoWindow;
 
         var pos = {
